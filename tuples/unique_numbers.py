f6718107ac6c401adcb0789f89b04b651009a042
MBEKE <ogadambeke@gmail.com>
2024-10-31 12:47:47 +0300

    refactor: add get_unique_elements function
    The function extracts unique elements from tuple
    Added doctring for beter readability and documentation

--- tuples/unique_numbers.py
+++ tuples/unique_numbers.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 
+def get_unique_elements(input_tuple):
+    """
+    Takes a tuple as input and returns a new tuple with unique elements.
+    
+    Parameters:
+        input_tuple (tuple): The tuple from which unique elements are to be extracted.
+    
+    Returns:
+        tuple: A new tuple containing only the unique elements of the input tuple.
+    """
+    unique_tuple = ()
+    for x in input_tuple:
+        if x not in unique_tuple:
+            unique_tuple += (x,)
+    return unique_tuple
+
+# Example usage
 tuple1 = (1, 9, 1, 6, 3, 4, 5, 3, 3, 4, 2, 8)
-tuple2 = ()
-for x in tuple1:
-   if x not in tuple2:
-      tuple2 += (x,)
+unique_tuple = get_unique_elements(tuple1)
 
 print("Original tuple:", tuple1)
-print("Unique numbers:", tuple2)
+print("Unique numbers:", unique_tuple)
