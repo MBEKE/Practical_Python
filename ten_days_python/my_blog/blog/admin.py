00107cd8b780170b95e99dbdb5f3c2af348511c5
MBEKE <ogadambeke@gmail.com>
2024-11-11 16:09:27 +0300

    Configure BlogPost model in Django admin for easy management
    - Customize BlogPostAdmin with list display, search, and filters
    - Register BlogPost model in admin for adding, editing, and deleting posts
    - Add sample posts via admin panel to test functionality

--- ten_days_python/my_blog/blog/admin.py
+++ ten_days_python/my_blog/blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import BlogPost
 
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('author', 'created_at')
+
 # Register your models here.
 admin.site.register(BlogPost)
