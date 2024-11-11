85bbca5923da991b3372841f77deb19532b85600
MBEKE <ogadambeke@gmail.com>
2024-11-11 19:10:03 +0300

    feat: Implement views and templates for blog homepage
    - Added views to handle homepage and post list display
    - Created basic templates for homepage layout and post listing
    - Configured URL routing to link views and templates
    - Tested end-to-end workflow from URL to data display
    
    Day 4 of #10DaysOfPython Django challenge: Building a functional homepage and connecting the frontend to backend views!

--- ten_days_python/my_blog/blog/views.py
+++ ten_days_python/my_blog/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
+def homepage(request):
+    posts = BlogPost.objects.all().order_by('-created_at')
+    return render(request, 'blog/homepage.html', {'posts': posts})
