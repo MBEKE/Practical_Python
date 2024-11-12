0c1846d87016cda1bee645d5df5c3bcc0a4d3506
MBEKE <ogadambeke@gmail.com>
2024-11-12 19:01:07 +0300

    feat: Add detailed post page with clickable titles
    - Implemented `post_detail` view to display full content of individual posts
    - Linked each post title on the homepage to its dedicated detail page
    - Updated URL routing to include dynamic post IDs
    - Enhanced interactivity by allowing visitors to explore posts in depth
    
    Day 5 of #10DaysOfPython Django challenge: Created a detailed post view to improve blog functionality and user experience!

--- ten_days_python/my_blog/blog/urls.py
+++ ten_days_python/my_blog/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         path('', views.homepage, name='homepage'),
+        path('post/<int:post_id>/', views.post_detail, name='post_detail')
         ]
--- ten_days_python/my_blog/blog/views.py
+++ ten_days_python/my_blog/blog/views.py
@@ -5,3 +5,9 @@ from .models import BlogPost
 def homepage(request):
     posts = BlogPost.objects.all().order_by('-created_at')
     return render(request, 'blog/homepage.html', {'posts': posts})
+
+
+# post-detail
+def post_detail(requests, post_id):
+    post = get_object_or_404(Post, id=post_id) # Retrieve the post by ID, or return 404 if not found
+    return render(request, 'blog/post_detail.html', {'post':  post})
